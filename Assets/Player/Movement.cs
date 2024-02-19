f2045828a219592146c8121bad8020f3a7a66b13
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2024-02-19 15:27:30 +0200

    Initial Commit

---
+++ Assets/Player/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Movement : MonoBehaviour
+    {
+        private void Start()
+        {
+        
+        }
+
+        private void Update()
+        {
+        
+        }
+    }
+}
+
