4bc089554fec99c15e7c69678025fb7d39d032a6
Ahmed Mustafa <61328457+devAhmedMustafa@users.noreply.github.com>
2024-02-19 16:08:22 +0200

    Static

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,32 @@ namespace Player
 {
     public class Movement : MonoBehaviour
     {
+        public Rigidbody2D PlayerRB;
+        public float moveX;
+        
         private void Start()
         {
-        
+            
         }
 
         private void Update()
         {
         
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+
+        private void HandleMovement()
+        {
+            moveX = Input.GetAxisRaw("Horizontal");
+        }
+        
+        private void Move()
+        {
+            PlayerRB.AddForce(new Vector2(1, 0), ForceMode2D.Impulse);
         }
     }
 }
